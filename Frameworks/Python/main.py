1b8998907df40c2551b706883dd1b2fa22ad0e46
Mohammad Kaif <shaikmohammadkaif73@gmail.com>
2025-05-31 12:25:29 +0530

    print file

--- Frameworks/Python/main.py
+++ Frameworks/Python/main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+from selenium import webdriver
+import time
+class TestYatra:
+    def test_yatra(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://www.yatra.com")
+        print("Yatra website opened successfully")
+        driver.maximize_window()
+        time.sleep(10)
+        driver.close()
+a = TestYatra()
+a.test_yatra()
